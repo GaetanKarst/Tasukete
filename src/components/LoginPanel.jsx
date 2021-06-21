20069ae96e1c7d0da60211851ea705354b59afd2
GaetanKarst <gaetan.karst@gmail.com>
2021-06-22 07:57:36 +0900

    Some cleaning

--- src/components/LoginPanel.jsx
+++ src/components/LoginPanel.jsx
@@ -1,27 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Redirect } from "react-router-dom";
 
 export default function LoginPanel({ users, currentUser, setCurrentUser, username, setUsername, password, setPassword }) {
 
-    const [userField, setUserField] = useState("")
-    const [passField, setPassField] = useState("")
-    const [passedCheck, setPassedCheck] = useState(false)
+    const [passedCheck, setPassedCheck] = useState(false);
 
     async function loginSubmitButton() {
-        const correctUser = users.filter(user => user.username === username && user.password === password)
-        console.log(correctUser)
-        if (correctUser.length === 0) { console.log("WRONG"); return alert("Error, please select matching username and password.") }
+        const registeredUser = users.filter(user => user.username === username && user.password === password);
+
+        if (registeredUser.length === 0) {
+            console.error("WRONG USER");
+            return alert("Incorrect, something went wrong with your username and/or password");
+        }
         else {
-            setCurrentUser(correctUser)
-            setPassedCheck(true)
+            setCurrentUser(registeredUser);
+            setPassedCheck(true);
         }
     }
-    /*
-        useEffect(() => {
-            currentUser.length !== 0;
-            console.log("trigger")
-        }, [currentUser])
-    */
+
     return passedCheck ? (
         <section>
             <Redirect to="/formpage" />
@@ -31,16 +27,28 @@ export default function LoginPanel({ users, currentUser, setCurrentUser, usernam
                 <div id="login-container">
                     <form>
                         <h1>Login</h1>
-                    <div>
-                        <label htmlFor="userField">Username </label>
-                        <br/>
-                        <input type="text" id="userField" name="userField" placeholder="Enter Username" maxLength="16" onChange={(e) => setUsername(e.target.value)} />
-                        <br />
-                        <label htmlFor="passField">Password</label>
-                        <br/>
-                        <input type="password" placeholder="Enter Password" id="passField" name="passField" maxLength="16" onChange={(e) => setPassword(e.target.value)} />
-                        <br />
-                   </div>
+                        <div>
+                            <label htmlFor="userField">Username </label>
+                            <br />
+                            <input
+                                type="text"
+                                id="userField"
+                                name="userField"
+                                placeholder="Enter Username"
+                                maxLength="16"
+                                onChange={(e) => setUsername(e.target.value)} />
+                            <br />
+                            <label htmlFor="passField">Password</label>
+                            <br />
+                            <input
+                                type="password"
+                                placeholder="Enter Password"
+                                id="passField"
+                                name="passField"
+                                maxLength="16"
+                                onChange={(e) => setPassword(e.target.value)} />
+                            <br />
+                        </div>
                     </form>
                     <button onClick={() => loginSubmitButton()}>Submit</button>
                 </div>
